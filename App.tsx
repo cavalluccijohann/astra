b94b9a6894d889ccb047d6fbc2dd5f20ad7b758e
HugoRCD <hugo.richard@epitech.eu>
2024-03-18 12:49:51 +0100

    switch all style to tailwind

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import { Camera, CameraType } from 'expo-camera';
-import React from 'react';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, Text, TouchableOpacity, View } from 'react-native';
 import { NativeWindStyleSheet } from "nativewind";
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import React from 'react';
 
 export default function App() {
     const [type, setType] = useState(CameraType.back);
@@ -16,8 +16,8 @@ export default function App() {
     if (!permission.granted) {
         // Camera permissions are not granted yet
         return (
-            <View style={styles.container}>
-                <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+            <View className="flex-1 items-center justify-center">
+                <Text className="text-center">We need your permission to show the camera</Text>
                 <Button onPress={requestPermission} title="grant permission" />
             </View>
         );
@@ -28,10 +28,10 @@ export default function App() {
     }
 
     return (
-        <View style={styles.container}>
-            <Camera style={styles.camera} type={type}>
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+        <View className="flex-1 justify-center">
+            <Camera type={type} className="flex-1">
+                <View className="flex-1 items-center justify-center bg-transparent">
+                    <TouchableOpacity onPress={toggleCameraType}>
                         <Text className="text-red-500 text-2xl">Flip Camera</Text>
                     </TouchableOpacity>
                 </View>
@@ -39,27 +39,6 @@ export default function App() {
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    camera: {
-        flex: 1,
-    },
-    buttonContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        backgroundColor: 'transparent',
-        margin: 64,
-    },
-    button: {
-        flex: 1,
-        alignSelf: 'flex-end',
-        alignItems: 'center',
-    },
-});
 
 NativeWindStyleSheet.setOutput({
     default: "native",
