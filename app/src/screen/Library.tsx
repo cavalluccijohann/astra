f7e145a987676a8049c198d8b26097ea337fef77
HugoRCD <hugo.richard@epitech.eu>
2024-04-02 17:16:38 +0200

    First user library

--- app/src/screen/Library.tsx
+++ app/src/screen/Library.tsx
@@ -1,48 +1,50 @@
-import { Alert, Button, Text, View } from 'react-native';
-import React from "react";
+import { Alert, Button, Image, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { List } from "react-native-paper";
+type Photo = {
+  id: string;
+  name: string;
+  url: string;
+};
 
 export default function Library() {
-  async function getUserAlbums() {
-    const authToken = await AsyncStorage.getItem('authToken');
-    try {
-      const response = await fetch('https://api.astra.hrcd.fr/album/user', {
-        method: 'GET',
-        headers: {
-          'Authorization': `Bearer ${ authToken }`,
-        },
-      });
-      console.log('response', response);
-      return await response.json();
-    } catch (error) {
-      console.log('error', error);
-      Alert.alert('Error fetching user albums');
-    }
-  }
-  async function getUserLibrary() {
-    const authToken = await AsyncStorage.getItem('authToken');
-    try {
-      const response = await fetch('https://api.astra.hrcd.fr/photo', {
-        method: 'GET',
-        headers: {
-          'Authorization': `Bearer ${ authToken }`,
-        },
-      });
-      console.log('response', response);
-      return await response.json();
-    } catch (error) {
-      console.log('error', error);
-      Alert.alert('Error fetching user library');
-    }
-  }
-  const albums = getUserAlbums();
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    const fetchUserLibrary = async () => {
+      const authToken = await AsyncStorage.getItem('authToken');
+      try {
+        const response = await fetch('https://api.astra.hrcd.fr/photo', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${authToken}`,
+          },
+        });
+        const data = await response.json();
+        setPhotos(data.content.photos);
+      } catch (error) {
+        console.log('Error fetching user library:', error);
+        Alert.alert('Error fetching user library');
+      }
+    };
+
+    fetchUserLibrary().then(r => r);
+  }, []);
+
   return (
-    <View className='p-10 m-20'>
-      <Text>Library</Text>
-      <Button title="Get User Albums" onPress={getUserAlbums} />
-      <Button title="Get User Library" onPress={getUserLibrary} />
-      <Text>{ JSON.stringify(albums) }</Text>
-      <Text>{ JSON.stringify(getUserLibrary()) }</Text>
+    <View className='flex-1 px-6 py-20'>
+      <Text className='text-2xl font-bold'>Library</Text>
+      <List.Subheader>Photos</List.Subheader>
+      <List.Section className='flex flex-wrap'>
+        {photos.map((photo, index) => (
+          <Image
+            key={index}
+            source={{ uri: photo.url }}
+            className='w-1/3 h-1/3'
+          />
+        ))}
+      </List.Section>
     </View>
   );
 }
