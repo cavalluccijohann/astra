f24429119032975b2beed302102b188ede4df029
johann <johann.cavallucci@epitech.eu>
2024-04-09 10:23:45 +0200

    FIX index.put.ts

--- api/server/routes/photo/[id]/index.put.ts
+++ api/server/routes/photo/[id]/index.put.ts
@@ -2,7 +2,8 @@ import { H3Event } from "h3";
 import { putPhotoInAlbum } from "../../../app/photoService";
 
 export default defineEventHandler(async (event: H3Event) => {
-    const albumId = readBody(event).albumId;
+    const body = await readBody(event);
+    const albumId = body.albumId;
     const photoId = getRouterParam(event, "id");
     await putPhotoInAlbum(photoId, albumId);
     return {
