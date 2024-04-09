38c4d013e9ac638a90da45339dcf18a0bf100f40
HugoRCD <hugo.richard@epitech.eu>
2024-04-09 11:42:32 +0200

    add a return

--- api/server/routes/album/[id]/index.put.ts
+++ api/server/routes/album/[id]/index.put.ts
@@ -3,6 +3,7 @@ import { H3Event } from "h3";
 
 export default defineEventHandler(async (event: H3Event) => {
   const user = event.context.user;
-  const photoId = getRouterParam(event, "id");
-  await toggleAlbumPrivacy(user, photoId);
+  const albumId = getRouterParam(event, "id");
+  await toggleAlbumPrivacy(user, albumId);
+  return { status: 200 };
 });
