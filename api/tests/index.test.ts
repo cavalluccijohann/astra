b2ed6b6c4f89924d61823b806773956884e6849d
HugoRCD <hugo.richard@epitech.eu>
2024-04-16 11:56:50 +0200

    Add Vitest

---
+++ api/tests/index.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest'
+import { $fetch } from 'nitro-test-utils/e2e'
+
+describe('API is alive', () => {
+  it('should return a 200 status', async () => {
+    const { data, status } = await $fetch('/')
+
+    expect(status).toBe(200)
+    expect(data).toMatchSnapshot()
+  })
+})
+
+describe('Feed route', () => {
+  it('should return a 200 status', async () => {
+    const response = await $fetch('/album', {
+      method: 'GET',
+    })
+
+    console.log(response.body)
+
+    // TODO
+  })
+})
